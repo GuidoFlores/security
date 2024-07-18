b58861e90473d1c22c9e5df24dde731b6e87dc6c
GuidoFlores <gjflores@espol.edu.ec>
2024-07-18 12:04:27 -0500

    vista del crud

--- routes/users.js
+++ routes/users.js
@@ -3,7 +3,8 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  /* 1. Renderización de la vista crud.ejs */
+  res.render('crud');
 });
 
 module.exports = router;
